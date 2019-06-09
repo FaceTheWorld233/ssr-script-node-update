859e72e71f2324041dbe60786affa5f9deb12d99
FaceTheWorld <23076338+FaceTheWorld233@users.noreply.github.com>
2019-06-09 11:20:44 +0800

    Update ss-panel-v3-mod.sh

--- ss-panel-v3-mod.sh
+++ ss-panel-v3-mod.sh
@@ -1,6 +1,45 @@
 #!/bin/bash
 #Check Root
 [ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
+install_ss_panel_mod_v3(){
+	yum -y remove httpd
+	yum install -y unzip zip git
+	num=$1
+	if [ "${num}" != "1" ]; then
+  	  wget -c --no-check-certificate https://raw.githubusercontent.com/cangshuking/ss-panel-and-ss-py-mu_cangshu/master/lnmp1.4.zip && unzip lnmp1.4.zip && rm -rf lnmp1.4.zip && cd lnmp1.4 && chmod +x install.sh && ./install.sh lnmp
+	fi
+	cd /home/wwwroot/
+	cp -r default/phpmyadmin/ .
+	cd default
+	rm -rf index.html
+	git clone https://github.com/cangshuking/ss-panel-v3-mod_cangshu.git tmp && mv tmp/.git . && rm -rf tmp && git reset --hard
+	cp config/.config.php.example config/.config.php
+	chattr -i .user.ini
+	mv .user.ini public
+	chown -R root:root *
+	chmod -R 777 *
+	chown -R www:www storage
+	chattr +i public/.user.ini
+	wget -N -P  /usr/local/nginx/conf/ --no-check-certificate https://raw.githubusercontent.com/cangshuking/ss-panel-and-ss-py-mu_cangshu/master/nginx.conf
+	service nginx restart
+	IPAddress=`wget http://whatismyip.akamai.com/ -O - -q ; echo`;
+	sed -i "s#103.74.192.11#${IPAddress}#" /home/wwwroot/default/sql/sspanel.sql
+	mysql -uroot -proot -e"create database sspanel;" 
+	mysql -uroot -proot -e"use sspanel;" 
+	mysql -uroot -proot sspanel < /home/wwwroot/default/sql/sspanel.sql
+	cd /home/wwwroot/default
+	php -n xcat initdownload
+	php xcat initQQWry
+	yum -y install vixie-cron crontabs
+	rm -rf /var/spool/cron/root
+	echo 'SHELL=/bin/bash' >> /var/spool/cron/root
+	echo 'PATH=/sbin:/bin:/usr/sbin:/usr/bin' >> /var/spool/cron/root
+	echo '*/20 * * * * /usr/sbin/ntpdate pool.ntp.org > /dev/null 2>&1' >> /var/spool/cron/root
+	echo '30 22 * * * php /home/wwwroot/default/xcat sendDiaryMail' >> /var/spool/cron/root
+	echo '0 0 * * * php /home/wwwroot/default/xcat dailyjob' >> /var/spool/cron/root
+	echo '*/1 * * * * php /home/wwwroot/default/xcat checkjob' >> /var/spool/cron/root
+	/sbin/service crond restart
+}
 Libtest(){
 	#自动选择下载节点
 	GIT='raw.githubusercontent.com'
@@ -97,7 +136,7 @@ install_centos_ssr(){
 	./configure && make -j2 && make install
 	echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
 	ldconfig
-	git clone -b master https://github.com/FaceTheWorld233/shadowsocks.git "/root/shadowsocks"
+	git clone -b manyuser https://github.com/FaceTheWorld233/shadowsocks.git "/root/shadowsocks"
 	cd /root/shadowsocks
 	chkconfig supervisord on
 	# 启用supervisord
@@ -149,7 +188,7 @@ install_ubuntu_ssr(){
 	pip install --upgrade pip
 	sudo pip install cymysql
 	cd /root
-	git clone -b master https://github.com/FaceTheWorld233/shadowsocks.git "/root/shadowsocks"
+	git clone -b manyuser https://github.com/FaceTheWorld233/shadowsocks.git "/root/shadowsocks"
 	cd shadowsocks
 	pip install -r requirements.txt
 	chmod +x *.sh
@@ -319,3 +358,105 @@ install_node_glzjin(){
 	wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
 	reboot now
 }
+install_panel_and_node(){
+	install_ss_panel_mod_v3 $1
+	# 取消文件数量限制
+	sed -i '$a * hard nofile 512000\n* soft nofile 512000' /etc/security/limits.conf
+	install_centos_ssr
+	wget -N -P  /root/shadowsocks/ --no-check-certificate  https://raw.githubusercontent.com/mmmwhy/ss-panel-and-ss-py-mu/master/userapiconfig.py
+	# 启用supervisord
+	echo_supervisord_conf > /etc/supervisord.conf
+  sed -i '$a [program:ssr]\ncommand = python /root/shadowsocks/server.py\nuser = root\nautostart = true\nautorestart = true' /etc/supervisord.conf
+	supervisord
+	#iptables
+	systemctl stop firewalld.service
+	systemctl disable firewalld.service
+	yum install iptables -y
+	iptables -F
+	iptables -X  
+	iptables -I INPUT -p tcp -m tcp --dport 22:65535 -j ACCEPT
+	iptables -I INPUT -p udp -m udp --dport 22:65535 -j ACCEPT
+	iptables-save >/etc/sysconfig/iptables
+	iptables-save >/etc/sysconfig/iptables
+	echo 'iptables-restore /etc/sysconfig/iptables' >> /etc/rc.local
+	echo "/usr/bin/supervisord -c /etc/supervisord.conf" >> /etc/rc.local
+	chmod +x /etc/rc.d/rc.local
+	echo "#############################################################"
+	echo "# 安装完成，登录http://${IPAddress}看看吧~                   #"
+	echo "# 用户名: 91vps 密码: 91vps                                  #"
+	echo "# phpmyadmin：http://${IPAddress}:888  用户名密码均为：root  #"
+	echo "# 安装完成，节点即将重启使配置生效                           #"
+	echo "# Github: https://github.com/mmmwhy/ss-panel-and-ss-py-mu    #"
+	echo "#############################################################"
+	reboot now
+}
+	echo
+	echo "#############################################################"
+	echo "# One click Install SS-panel and Shadowsocks-Py-Mu          #"
+	echo "# fork: https://github.com/mmmwhy/ss-panel-and-ss-py-mu     #"
+	echo "# Author: 91vps, dumplin                                    #"
+	echo "# Please choose the server you want                         #"
+	echo "# 1  SS-V3_mod_panel and node One click Install             #"
+	echo "# 2  SS-node_webapi One click Install                       #"
+	echo "# 3  SS-node_glzjin One click Install                       #"
+	echo "#############################################################"
+	echo
+ARGS=`getopt --long mode:,UserNODE_ID:,MYSQL_HOST:,MYSQL_USER:,MYSQL_PASS:,MYSQL_DB: -- "$@"`
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+while true;
+do
+    case "$1" in
+	--mode)  
+		mode=$2  
+		shift 2
+		;;
+	--UserNODE_ID)  
+		UserNODE_ID=$2  
+		shift 2
+		;;
+	--MYSQL_HOST)  
+		MYSQL_HOST=$2  
+		shift 2
+		;;   
+	--MYSQL_USER)  
+		MYSQL_USER=$2  
+		shift 2
+		;;			   
+	--MYSQL_PASS)  
+    	MYSQL_PASS=$2  
+    	shift 2
+        ;;   
+	--MYSQL_DB)  
+    	MYSQL_DB=$2
+    	shift 2
+		;; 
+    --)
+        shift
+        break
+        ;;
+    *) 
+        break
+        ;;
+esac
+done
+num=$1
+if [ "${num}" == "1" ]; then
+    install_panel_and_node 1
+else
+    #stty erase '^H' && read -p " 请输入数字 [1-2]:" num
+    num=$mode
+		case "$num" in
+		1)
+		install_panel_and_node
+		;;
+		2)
+		install_node
+		;;
+		3)
+		install_node_glzjin
+		;;
+		*)
+		echo "请输入正确数字 [1-3]"
+		;;
+	esac
+fi
